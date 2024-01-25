a36c0a7b1e96b9083dc3eeff85c9c11a3a597758
Tomasz Ducin <tomasz@ducin.it>
2024-01-25 18:55:46 +0100

    styling improvements

--- templates/subscribe-tpl.js
+++ templates/subscribe-tpl.js
@@ -2,7 +2,7 @@ const subscribeTpl = () => {
     return `<div class="email-subscribe">
     <h4 class="subscribe-header">Get notified about blog posts, recordings and workshops!</h4>
     <p>Subscribe to Ducin.dev and you'll get notified when new content is published. Expect in-depth posts, recordings and online workshops. The topics include: JS/TS, Angular, React and related tech.</p>
-    <div style="text-align: left" class="sender-form-field" data-sender-form-id="lrt2rokxero62a36uep"></div>
+    <div class="sender-form-field" data-sender-form-id="lrt2rokxero62a36uep"></div>
   </div>`
 }
 
