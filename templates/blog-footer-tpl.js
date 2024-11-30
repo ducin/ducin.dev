06fd3df5115929ba02f192ed308b25cdc20c13fc
Tomasz Ducin <tomasz@ducin.dev>
2024-12-01 00:18:52 +0100

    update

--- templates/blog-footer-tpl.js
+++ templates/blog-footer-tpl.js
@@ -3,11 +3,11 @@ const blogFooterTpl = () => {
     <p>I'm an Independent Consultant, Architect and a Developer. Speaker at European and Polish conferences. A trainer with passion to explain how things work and to avoid overcomplicated solutions. A critic of buzzwords and overexcitement around certain tools. Concentrated on solving organizational and technical project issues. Loves working with people. Ex-theatre actor. 4 espressos a day.
     </p>
     <p><a href="mailto:tomasz@ducin.dev">Contact Tomasz</a> for information about <a href="/trainings">trainings on JavaScript, TypeScript, React, Angular and Architecture.</a></p>
-    <p>
-        <a target="_blank" href="http://twitter.com/tomasz_ducin" class="icon fa-twitter twitter-color">
-            <span class="label">Twitter</span>
-            Follow me on X/twitter for tech-related updates.
-        </a>
+    <p>Follow me on
+        <a target="_blank" href="https://bsky.app/profile/ducin.dev">bluesky 🦋</a>,
+        <a target="_blank" href="https://www.linkedin.com/in/tomasz-ducin-82234a4b/">LinkedIn</a>,
+        or <a target="_blank" href="http://x.com/tomasz_ducin">the e𝕏 platform</a>
+        for tech-related updates.
     </p>
 </div>`
 }
